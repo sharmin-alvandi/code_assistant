a8488b73286cd8d705ef5c7e063c9ae9ab6ebbd6
sharmin-alvandi <sha.alvandi1@gmail.com>
2024-02-29 00:11:41 -0500

    Add js,css and html editors plus an iframe to show the code result

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,64 @@
+import { useEffect, useState } from "react";
+import Editor from "./Editor";
+
 function App() {
-  return <div></div>;
+  const [html, setHtml] = useState("");
+  const [css, setCss] = useState("");
+  const [js, setJs] = useState("");
+  const [srcDoc, setSrcDoc] = useState("");
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setSrcDoc(`
+  <html>
+  <style>${css}</style>
+    <body>
+      ${html}
+    </body>
+    <script> ${js}</script>
+    <script type="text/ruby">
+    puts Time.now
+  </script>
+  </html>`);
+    }, 250);
+    return () => clearTimeout(timeOut);
+  }, [html, js, css]);
+
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="Javascript"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          style={{ border: 0 }}
+          //frameBorder="0" is deprecated
+          width="100%"
+          height="100%"
+        ></iframe>
+      </div>
+    </>
+  );
 }
 
 export default App;
